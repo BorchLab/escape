05979ffa4e6c8617aa4a95d58b9e5168fb605cc3
theHumanBorch <ncborch@gmail.com>
2025-05-11 14:42:34 -0500

    Update escape.gene.sets.R
    Expand gene set information

--- R/escape.gene.sets.R
+++ R/escape.gene.sets.R
@@ -1,9 +1,24 @@
 #' Built-In Gene Sets for escape  
-#' 
-#' A list of gene sets derived from Azizi, et al 2018 
-#' \href{https://pubmed.ncbi.nlm.nih.gov/29961579/}{PMID: 29961579})
-#' relating to tumor immunity.
+#'
+#' `escape.gene.sets` ships with **escape** and provides a convenient set of
+#' cell-type and pathway signatures from the scRNA-seq tumour micro-environment
+#' study by Azizi *et al.* (2018, Cell \doi{10.1016/j.cell.2018.06.021}).  These
+#' signatures capture major immune and stromal populations observed across
+#' breast-cancer samples and serve as a lightweight default for quick testing or
+#' exploratory analyses.
+#
+#' @details
+#' The original paper defined cell-type signatures as the top differentially
+#' expressed genes per cluster (Azizi *et al.*, Supplementary Table S3).  
+#'
+#' @usage data("escape.gene.sets")
+#' @seealso [runEscape()], [escape.matrix()], [getGeneSets()]
+#' @keywords datasets
+#' @source Supplementary Table S3 in Azizi *et al.* (2018)  
+#'         <https://pubmed.ncbi.nlm.nih.gov/29961579/>
+#' @references
+#' Azizi E, *et al.* **Single-cell map of diverse immune phenotypes in the
+#' breast tumour microenvironment.** *Cell* 173(5):1293-1308 (2018).
 #' @docType data
 #' @name escape.gene.sets
-#' 
 NULL
