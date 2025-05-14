03f1f265eb98ea911cb5979bd28fece0378213c1
theHumanBorch <ncborch@gmail.com>
2025-05-14 07:31:29 -0500

    Update escape.gene.sets.R

--- R/escape.gene.sets.R
+++ R/escape.gene.sets.R
@@ -1,7 +1,7 @@
 #' Built-In Gene Sets for escape  
 #'
 #' `escape.gene.sets` ships with **escape** and provides a convenient set of
-#' cell-type and pathway signatures from the scRNA-seq tumour micro-environment
+#' cell-type and pathway signatures from the scRNA-seq tumor micro-environment
 #' study by Azizi *et al.* (2018, Cell \doi{10.1016/j.cell.2018.06.021}). These
 #' signatures capture major immune and stromal populations observed across
 #' breast-cancer samples and serve as a lightweight default for quick testing or
@@ -18,7 +18,7 @@
 #'         <https://pubmed.ncbi.nlm.nih.gov/29961579/>
 #' @references
 #' Azizi E, *et al.* **Single-cell map of diverse immune phenotypes in the
-#' breast tumour microenvironment.** *Cell* 173(5):1293-1308 (2018).
+#' breast tumor microenvironment.** *Cell* 173(5):1293-1308 (2018).
 #' @docType data
 #' @name escape.gene.sets
 NULL
